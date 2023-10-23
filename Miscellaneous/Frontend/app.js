e0f337f0c7c98030689f5b477fa12aa7e72eba9d
Prajwal Gandhi <1000015996@dit.edu.in>
2023-10-24 00:28:06 +0530

    Micellaneous Concept Completed

--- Miscellaneous/Frontend/app.js
+++ Miscellaneous/Frontend/app.js
@@ -27,7 +27,78 @@
 // };
 
 // BY USING OOPS CONCEPTS.
-let arr = [1,2,3];
-arr.sayHello = ()=>{
-    console.log("Hello!, i am arr");
+// let arr = [1,2,3];
+// arr.sayHello = ()=>{
+//     console.log("Hello!, i am arr");
+// }
+
+// Factory Function
+function PersonMaker(name,age){
+    const person = {
+        name: name,
+        age: age,
+        talk() {
+            console.log(`Hi, my name is ${this.name}`);
+        },
+    };
+    return person;
+};
+
+// Constructors - doesn't return anything & start with capital
+// function Person(name,age){
+//     this.name = name;
+//     this.age = age;
+// }
+
+// Person.prototype.talk = function(){
+//     console.log(`Hi, my name is ${this.name}`);
+// }
+
+// let p1 = new Person("Adam",25);
+// let p2 = new Person("Kesgv",24);
+
+// Classes
+// class Person{
+//     constructor (name,age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     talk(){
+//         console.log(`Hi, my name is ${name}`);
+//     }
+// }
+
+// let p3 = new Person("Adam",25);
+// let p4 = new Person("Kesgv",24);
+
+// Inheritance
+
+class Person{
+    constructor(name,age){
+        console.log("person class constructor");
+        this.name = name;
+        this.age = age;
+    }
+    talk(){
+        console.log(`Hi, I am ${this.name}`);
+    }
 }
+class Student extends Person{
+    constructor(names,age,marks){
+        console.log("student class constructor");
+        super(name,age);  // parent class constructor is being called
+        this.marks = marks;
+    }
+    
+};
+
+class Teacher extends Person{
+    constructor(names,age,subject){
+        super(name,age);     // parent class constructor is being called
+        this.subject = subject;
+    }
+    talk(){
+        console.log(`Hi, I am ${this.name}`);
+    }
+};
+
